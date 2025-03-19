b9983da3428ba08e47d9647329f2d2f30e0437d2
Artem.Mokeev <artmokeev274@gmail.com>
2025-03-19 11:02:49 +0300

    added features component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero'
 import About from './components/About'
+import Features from './components/Features'
 
 function App() {
   return (
     <main className="overflow-x-hidden">
       <Hero />
       <About />
+      <Features />
       {/* <section className="w-screen h-screen"></section> */}
     </main>
   )
---
+++ src/components/Features.jsx
@@ -0,0 +1,18 @@
+function Features() {
+  return (
+    <section id="features" className="bg-black">
+      <div className="relative px-5 tablet:px-8 laptop:container laptop:mx-auto">
+        <div className="font-circular-web leading-5 text-sm pt-16 max-w-md laptop:pt-32">
+          <p className="text-white">Explore the Elden Ring Universe</p>
+          <p className="text-gray-500">
+            Immerse yourself in a rich and ever-expanding universe where a
+            vibrant array of products converge into an interconnected overlay
+            experience on your world.
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Features
